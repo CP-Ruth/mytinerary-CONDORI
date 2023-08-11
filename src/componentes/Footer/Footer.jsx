2ab6f9c34f7fb2abd1bfa90dfff665da205dfb01
Ruth <elith.tati@gmail.com>
2023-08-11 15:51:12 -0300

    :|

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { IoLogoWhatsapp, IoLogoLinkedin, IoLogoGithub, IoLogoInstagram} from "react-icons/io5";
+import "../Footer/styles.css"
 
 function Footer() {
   return (
     <footer>
-      <p>Ruth</p>
+      <div className='socialmedia'>
+      <a href="https://web.whatsapp.com"><IoLogoWhatsapp/></a>
+      <a href="https://www.instagram.com"><IoLogoInstagram/></a>
+      <a href="https://github.com/listo-dale/mytinerary-CONDORI"><IoLogoGithub/></a>
+      <a href="https://ar.linkedin.com"><IoLogoLinkedin/></a>
+      </div>
+
+      <h3>MindHub - Ruth Condorí</h3>
     </footer>
   )
 }
--- src/pages-vistas/Home.jsx
+++ src/pages-vistas/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io"
-import './styles.css'
+import '../pages-vistas/styles.css'
 
 
 function Home() {
@@ -12,33 +12,33 @@ function Home() {
     { href: 'src/assets/escocia.jpg', namepais: 'Escocia' }
   ]
 
+  const cantidad = img.length;
   const [imagenActual, setImagenActual] = React.useState(0);
-  //const cantidad = img.length;
-  //useEffect(() => {  }, [])
+
+  const handlerNext = () => {
+    setImagenActual(imagenActual === cantidad - 1 ? 0 : imagenActual + 1);
+  }
+
+  const handlerBack = () => {
+    setImagenActual(imagenActual === cantidad - 1 ? 0 : imagenActual - 1);
+  }
 
 
   return (
     <>
+    <h2 className='carruseltitle'>Popular Mytineraries</h2>
       <div className="container">
         <button onClick={() => handlerBack()}><IoIosArrowBack />Back</button>
-
         {img.map((each, key) => {
           return (
             <div>
-               {imagenActual === key && <img key={key} src={each.href} alt={each.namepais}/>}
+              {imagenActual === key && <img className="carrusel" key={key} src={each.href} alt={each.namepais} />}
             </div>
           )
         }
-
         )}
-
-
         <button onClick={() => handlerNext()}><IoIosArrowForward />Next</button>
       </div>
-
-
-
-
 
     </>
   )
